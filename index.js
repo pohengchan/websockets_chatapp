e8cae66c8b23fff9a7ed335627e57d1fdc2d1343
pohengchan <pohengchan@gmail.com>
2023-01-18 21:49:22 +0100

    add username functionality

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const app = express();
+    //webserver 
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    //get request and response object
+app.get('/FemcoderHome', (req, res) => {
+    //node.js current file path
+//     res.sendFile(__dirname + '/index.html');
+    res.send('<h1>Hello all Femcoders!</h1>')
 });
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
-  });
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+//    socket.on('disconnect', () => {
+//         console.log('user disconnected');
+//     });
+//     socket.on('chat message', (data) => {
+//         console.log(data.name + "'s message: " + data.message );
+//         io.emit('chat message', {name:data.name, msg: data.message});
+//     });
+//     });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
