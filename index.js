50a5df6f12a133097279fa299e79165f3dce46e9
pohengchan <pohengchan@gmail.com>
2023-01-18 21:56:36 +0100

    refactor io code

--- index.js
+++ index.js
@@ -3,26 +3,26 @@ const app = express();
     //webserver 
 const http = require('http');
 const server = http.createServer(app);
-// const { Server } = require("socket.io");
-// const io = new Server(server);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
     //get request and response object
 app.get('/FemcoderHome', (req, res) => {
     //node.js current file path
-//     res.sendFile(__dirname + '/index.html');
-    res.send('<h1>Hello all Femcoders!</h1>')
+     res.sendFile(__dirname + '/index.html');
+//    res.send('<h1>Hello all Femcoders!</h1>')
 });
 
-// io.on('connection', (socket) => {
-//     console.log('a user connected');
-//    socket.on('disconnect', () => {
-//         console.log('user disconnected');
-//     });
-//     socket.on('chat message', (data) => {
-//         console.log(data.name + "'s message: " + data.message );
-//         io.emit('chat message', {name:data.name, msg: data.message});
-//     });
-//     });
+io.on('connection', (socket) => {
+    console.log('a user connected');
+   socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (data) => {
+        console.log(data.name + "'s message: " + data.message );
+        io.emit('chat message', {name:data.name, msg: data.message});
+    });
+    });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
